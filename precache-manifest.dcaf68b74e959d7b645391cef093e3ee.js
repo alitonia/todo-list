4b48a8c3c7d29dc7beae90ab10ee6348fd8f28a2
Zen-net <allyofjustice1@gmail.com>
2020-07-03 14:46:07 +0700

    Updates

---
+++ precache-manifest.dcaf68b74e959d7b645391cef093e3ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7d5c06d7c1f31ddd41d4813e53f516",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "93580c5b8ec1cde4f09e",
+    "url": "/todo-list/static/js/2.6f748482.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.6f748482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "577346b24ea3b3ba5ce6",
+    "url": "/todo-list/static/js/main.22b55168.chunk.js"
+  },
+  {
+    "revision": "7b15bc2f73099ab4ac07",
+    "url": "/todo-list/static/js/runtime-main.21a12556.js"
+  }
+]);
